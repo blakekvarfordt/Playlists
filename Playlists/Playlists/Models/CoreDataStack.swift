85559c51a2cbdfdab29f367d76e1ad78136759d5
Blake Kvarfordt <blakekvarfordt@gmail.com>
2019-08-06 22:34:17 -0600

    sets up core data model and core data stack

---
+++ Playlists/Playlists/Models/CoreDataStack.swift
@@ -0,0 +1,25 @@
+//
+//  CoreDataStack.swift
+//  Playlists
+//
+//  Created by Blake kvarfordt on 8/6/19.
+//  Copyright © 2019 Blake kvarfordt. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class CoreDataStack {
+    
+    static let container: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "Playlists")
+        container.loadPersistentStores() { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        }
+        return container
+    }()
+    
+    static var context: NSManagedObjectContext { return container.viewContext}
+}
---
+++ Playlists/Playlists/Models/Playlist+Convenience.swift
@@ -0,0 +1,16 @@
+//
+//  Playlist+Convenience.swift
+//  Playlists
+//
+//  Created by Blake kvarfordt on 8/6/19.
+//  Copyright © 2019 Blake kvarfordt. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+extension Playlist {
+    convenience init(name: String, context: NSManagedObjectContext) {
+        
+    }
+}
